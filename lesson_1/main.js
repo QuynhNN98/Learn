1617eff7d181378eb37bae43f8188f7209f264b2
QuynhNN98 <nguyenngocquynh926@gmail.com>
2023-02-19 11:18:00 +0700

    fist

---
+++ lesson_1/main.js
@@ -0,0 +1,146 @@
+var fullName = 'Nguyễn Ngọc Quỳnh';
+var agg = '25';
+// alert(fullName)
+// alert(agg)
+//Coment 1 dòng
+/* Commet nhiều dòng*/
+/*
+    //Các hàm built-in cơ bản
+1. Alert
+Bật lên hộp thoại(OK)
+2. Console
+
+3. Confirm
+Bật lên hộp message (Cancel or OK)
+4. Prompt
+Kết hợp của hàm alert và hàm confirm và thêm textbox để chúng ta nhập thông tin vào
+5. Set timeout 
+Bật lên hộp thông báo và chỉ chạy một lần
+6. Set interval
+Bật lên hộp thông báo và chạy nhiều lần sau bn thời gian mà chúng ta đặt
+*/
+
+
+// console.log(fullName);
+// confirm('xac nhan ban la nu');
+// prompt('ban bao nhieu tuoi');
+// setTimeout(function(){alert('Thong bao')},1000);
+// setInteval(function(){alert('đây là một thông báo')},1000);
+
+
+    /* Toán tủ số học
++       --> Cộng
+-       --> Trừ
+*       --> Nhân
+**      --> Lũy thừa
+/       --> Chia
+%       --> Chia lấy số dư
+++      --> Tăng 1 giá trị số
+--      --> Giảm 1 giá trị số
+*/
+// VD 
+
+// var a = 2;
+// var b = 4;
+// var c = a + b;
+// c++;
+// c++;
+// console.log(c);
+
+
+    /* Toán tử gán
+
+Toán tử     Ví dụ       Tương đương
+=           x = y       x = y
++=          x += y      x = x + y
+-=          x -= y      x = x - y
+*=          x *= y      x = x * y
+/=          x /= y      x = x / y
+**=         x **= y     x = x ** y 
+
+*/
+//VD
+
+// var a = 2;
+// a **= 1;
+// console.log(a);
+ 
+    /* Toán tử ++ --
+Prefix (Tiền tố)
+- Thực hiện cộng/trừ 1 cho hàm số và trả về kết quả sau khi cộng/ trừ
+Postfix (Hậu tố)
+- Thực hiện cộng trừ 1 cho hàm số và trả về kết quả trước khi cộng/ trừ
+*/
+//VD
+
+
+// var a = 1;
+// var b = 2;
+// console.log(++a + a++);//4
+// console.log(--b + b--);//2
+
+    // Toán tử chuỗi
+//VD
+
+// var firtname = ('Nguyen');
+// var lastname = ('Quynh');
+// console.log(firtname,lastname );
+// console.log(firtname + ' '+ lastname);
+
+
+    /* Toán tử so sánh
+
+==      -->     Bằng
+!=      -->     Không bằng
+>       -->     Lớn hơn
+<       -->     Nhỏ hơn
+>=      -->     Lớn hơn hoặc bằng
+<=      -->     Nhỏ hơn hoặc bằng */
+
+
+// var a = 5;
+// var b = 7;
+// if (a > b) {
+//     console.log('a lớn hơn b')
+    
+// } else {console.log('a nhỏ hơn b')
+    
+// }
+    /*Kiểu dữ liêu Boolean 
+*/
+// var a = 1;
+// var b = 2;
+// var inSuccess = a > b;
+
+// console.log (inSuccess);
+// var age = 16;
+// var age2 = 18;
+// var canBuyAlcohol = age >= age2;
+// console.log(canBuyAlcohol);
+
+    /* Câu lệnh If -else*/
+    // var a = 5;
+// var b = 7;
+// if (a > b) {
+//     console.log('a lớn hơn b')
+    
+// } else {console.log('a nhỏ hơn b')
+    /*Hàm (function) trong Javascript
+    1. Hàm?
+    - Một khối mã
+    - Làm một việc cụ thể
+    2. Loại hàm
+    - Built-in
+    - Tự định nghĩa
+    3. Tính chất
+    - Không thực thi khi định nghĩa
+    - Sẽ thực thi khi được gọi
+    - Có thể nhận tham số
+    - Có thể trả về 1 giá trị
+    4. Tạo hàm đầu tiên
+
+    */
+   // a-z A-Z 0-0;
+   //function  tên
+
+
